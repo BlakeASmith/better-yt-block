90ce946d4574504c14ff5d0d7b2e24589e0c3868
Blake Smith <blakeinvictoria@gmail.com>
2022-04-13 00:29:50 -0700

    feat: fetch videos from reccomended after page load

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-alert("Hello from Better Youtube Blocking")
+
+chrome.runtime.onMessage.addListener((req, sender, sendResp) => {
+    if (req.message === "clicked_icon") {
+        alert("clicked icon")
+    }
+
+    if (req.message === "page_load") {
+        let contents = $("#contents")
+        if (contents)
+            contents.remove()
+        //let videos = $("#contents > ytd-compact-video-renderer");
+    }
+});
+
