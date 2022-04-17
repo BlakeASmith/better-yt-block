16115687f28167db436d87097f1cb61e83affe9b
Blake Smith <blakeinvictoria@gmail.com>
2022-04-17 07:26:11 -0700

    chore: update config to use JSX

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,41 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-   mode: "production",
-   entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
-      content: path.resolve(__dirname, "..", "src", "content.ts" ),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
-   },
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, "..", "src", "background.ts"),
+    content: path.resolve(__dirname, "..", "src", "content.tsx"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".tsx"],
+  },
   devtool: "source-map",
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
-   },
-   plugins: [
-      new CopyPlugin({
-        patterns: [{from: ".", to: ".", context: "public"}]
-      }),
-   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: ".", context: "public" }],
+    }),
+  ],
 };
